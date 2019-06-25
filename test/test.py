958c506f6a27238f2e8916d029e4a08ed4d4f6a2
Amplexis <kevin.hamlin@gmail.com>
2019-06-25 19:33:45 -0400

    Passed third validate_time test

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 from src.models.time import validate_time
 
 
-def test__should_return_true_and_int_of_17_when_validating_5_pm():
+def test__validate_time_should_return_true_and_int_of_17_when_validating_5_pm():
     time = "5 pm"
 
     results = validate_time(time)
@@ -12,7 +12,7 @@ def test__should_return_true_and_int_of_17_when_validating_5_pm():
     assert time_int == 17
 
 
-def test__should_return_false_and_time_out_of_range_when_validating_4_pm():
+def test__validate_time_should_return_false_and_time_out_of_range_when_validating_4_pm():
     time = "4 pm"
 
     results = validate_time(time)
@@ -23,3 +23,10 @@ def test__should_return_false_and_time_out_of_range_when_validating_4_pm():
     assert time_int == "Time is out of range"
 
 
+def test__validate_time_should_return_true_when_validating_12_am():
+    time = "12 am"
+
+    results = validate_time(time)
+    valid_time = results[0]
+
+    assert valid_time
